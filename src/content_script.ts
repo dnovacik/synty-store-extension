483647396b34ff74d2cd1d7cc28ebef061e2cdf6
DeathVenom54 <fdragon602@gmail.com>
2021-11-21 23:45:01 +0530

    Test for Unity assetstore

--- src/content_script.ts
+++ src/content_script.ts
@@ -1,7 +1,22 @@
-const MSG_EXAMPLE = "EXAMPLE";
+chrome.runtime.onMessage.addListener(function (message) {
+  if (message === "syncUAS") {
+    const titles = document.querySelectorAll("._161YN._30Ec_");
 
-chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-  if (msg === MSG_EXAMPLE) {
-    sendResponse(`${MSG_EXAMPLE} received`)
+    let names = Array.from(titles.values()).map((title) => {
+      const name = title.textContent?.toLowerCase();
+      if (!name) return "";
+      return name.startsWith("polygon") || name.startsWith("simple")
+        ? name
+        : "";
+    });
+    names = names.filter((n) => (!n ? "" : n));
+
+    const existing = JSON.parse(
+      localStorage.getItem("uasAssets") || "[]"
+    ) as string[];
+    names = Array.from(new Set(existing.concat(names)));
+
+    localStorage.setItem("uasAssets", JSON.stringify(names));
+    console.log("Saved to LC");
   }
-})
+});
