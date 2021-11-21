ba13a38b3d2265f6b33e0888004ccf57fc5a2324
ra6e <novacik.daniel@gmail.com>
2021-11-21 13:52:57 +0100

    initial commit

---
+++ src/options.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export const Options = ({ }) => {
+  return (
+    <>
+      <div>Options Example</div>
+    </>
+  );
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Options />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
---
+++ src/popup.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export const Popup = ({ }) => {
+  return (
+    <>
+      <div>Example</div>
+    </>
+  );
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Popup />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
